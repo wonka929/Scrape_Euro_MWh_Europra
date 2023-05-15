8787fbaa157c2d6cc2ad5943b936032a0857276e
wonka929 <wonka929@gmail.com>
2023-05-15 10:16:18 +0200

    Bypass dati mancanti

--- Dati Euro-MWh Europra.py
+++ Dati Euro-MWh Europra.py
@@ -4,6 +4,7 @@ from tqdm.notebook import tqdm
 import datetime
 import warnings
 from matplotlib import pyplot as plt
+import requests
 warnings.filterwarnings('ignore')
 
 # %%
@@ -13,16 +14,24 @@ def data_range(start, end):
     date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
     return date_generated
 
-days = data_range('2022-01-01', '2023-01-01')
 
+day = data_range('2021-06-01', datetime.datetime.now().strftime('%Y-%m-%d'))
+days = [d for d in reversed(day)]
 
-# %%
 cc = pd.DataFrame()
 
 for elem in tqdm(days):
-    data = datetime.datetime.strftime(elem, '%Y-%m-%d')
-    aa = aa = pd.DataFrame(pd.read_html('https://euenergy.live/?date=' + data)[0]).drop(["Unnamed: 1"], axis=1).rename(columns={"Unnamed: 0": "Country"}).rename(columns={"€/MWh": elem}).set_index("Country", drop=True)
-    cc = pd.concat([cc, aa], axis=1)
+    try:
+        data = datetime.datetime.strftime(elem, '%Y-%m-%d')
+        aa = pd.DataFrame(pd.read_html(requests.get('https://euenergy.live/?date=' + data).text)[0]).drop(["Unnamed: 1"], axis=1).rename(columns={"Date:": "Country"}).rename(columns={"€/MWh": elem}).set_index("Country", drop=True)
+        aa = aa.groupby('Country').mean()
+        
+        cc = pd.concat([cc, aa], axis=1, join='outer')
+    except Exception as e:
+        print(e)
+        continue
+
+#cc.dropna(axis=1, inplace=True)
 
 # %%
 cc.T[["🇮🇹 Italy", "🇩🇪 Germany","🇫🇷 France"]].describe().plot(kind='bar', figsize=(20, 10)).figure.savefig('plot_bar.png')
@@ -31,9 +40,6 @@ cc.T[["🇮🇹 Italy", "🇩🇪 Germany","🇫🇷 France"]].describe().plot(k
 cc.T[["🇮🇹 Italy", "🇩🇪 Germany", "🇫🇷 France"]].plot(figsize=(20, 10)).figure.savefig('plot.png')
 
 # %%
-cc.T.describe().loc['mean'].plot(kind='bar', figsize=(20, 10)).figure.savefig('stats.png')
-
-# %%
-
+cc.T.describe().loc['mean'][:-1].plot(kind='bar', figsize=(20, 10)).figure.savefig('stats.png')
 
 
